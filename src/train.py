bd72dd61035f7a7b689f69db3b5901cafd27fde8
meghxroy <165138599+meghxroy@users.noreply.github.com>
2025-08-23 16:44:14 +0530

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv1D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import LayerNormalization
+from tensorflow.keras.layers import MultiHeadAttention
+from tensorflow.keras.layers import Add
+from tensorflow.keras.layers import Activation
+from tensorflow.keras import layers,models
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from model import 
